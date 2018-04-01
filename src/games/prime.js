d9d483d01fb5ad3a9c0dcb98607384f6c94319d4
Klimonov <22pov22@gmail.com>
2018-04-01 14:14:12 +0300

    step 9. make Prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,9 +2,19 @@ import { game, getRandomNumber } from '..';
 
 const termsOfTheGame = 'Is this number prime?';
 
+
+const isPrime = (randomNumber) => {
+  const findingDivisor = (num, counter) => {
+    if (randomNumber % counter !== 0) return findingDivisor(num, counter + 1);
+    return counter === randomNumber;
+  };
+  return randomNumber === 1 ? true : findingDivisor(randomNumber, 2);
+};
+
 const findCorrectAnswerAndQuestion = () => {
   const randomNumber = getRandomNumber(100);
-  const correctAnswer = (randomNumber % 2 === 0 ? 'yes' : 'no');
+  const correctAnswer = (isPrime(randomNumber) ? 'yes' : 'no');
+  console.log(correctAnswer);
   return [correctAnswer, randomNumber];
 };
 
